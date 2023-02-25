e464060c462f52b8b593a50dd96bbda8628c0038
Radu Miron <radu.miron@aut.utcluj.ro>
2023-02-25 16:48:56 +0200

    lab 4 2023 - draft impl
    Signed-off-by: Radu Miron <radu.miron@aut.utcluj.ro>

---
+++ src/main/java/aut/isp/lab4/exercise1/AquariumController.java
@@ -0,0 +1,8 @@
+package aut.isp.lab4.exercise1;
+
+public class AquariumController {
+
+    //attributs
+    //constructors
+    //methods
+}
---
+++ src/main/java/aut/isp/lab4/exercise1/Exercise1.java
@@ -0,0 +1,8 @@
+package aut.isp.lab4.exercise1;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        //create an object
+    }
+}
